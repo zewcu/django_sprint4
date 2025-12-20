5fbbc3f015099cc664d6e3fa9ae842833a1d9184
Grisha <154027065+zewcu@users.noreply.github.com>
2025-12-21 02:08:29 +0300

    Add files via upload

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d5y4uhe-yu*a6rryj0h$t374=t9_b-b$ujvfd-=kuwc%_uksuh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
