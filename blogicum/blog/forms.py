06742ca313244bb9bbaa26445fedd71924704f02
Grisha <154027065+zewcu@users.noreply.github.com>
2026-03-20 21:18:00 +0300

    Update forms.py

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth import get_user_model
-from .models import Comment
+from .models import Comment, Post
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('author', 'created_at')
 
 
 class UserUpdateForm(UserChangeForm):
